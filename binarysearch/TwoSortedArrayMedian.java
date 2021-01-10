33e34ad6a84caf8e6fba4f222993213877923ee9
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2021-01-10 18:25:36 +0800

    [Binary Search] Added sample data for clarification in TwoSortedArrayMedian

--- binarysearch/TwoSortedArrayMedian.java
+++ binarysearch/TwoSortedArrayMedian.java
@@ -57,6 +57,24 @@ public class TwoSortedArrayMedian {
         }
     }
 
+    /* Additional Clarifications:
+    * Sample Data that will lead to the first if condition:
+    *   A = [1]
+    *   B = [2, 3] 
+    *
+    * Sample Data that will lead to the second if condition:
+    *   A = [2, 3]
+    *   B = [1]
+    *
+    * Sample Data that will lead to the third if condition:
+    *   A = [3]
+    *   B = [1, 2]
+    *
+    *   or
+    *
+    *   A = [2]
+    *   B = [1, 3] */
+
     public int findKthHelper(int[] A, int aStart, int[] B, int bStart, int k) {
         if (aStart >= A.length) {
             return B[bStart + k - 1];
